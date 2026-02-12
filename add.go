01a6fee2309a6c6351b716fdddf71738c14b9059
piyushyadav <yadavpiyush222@gmail.com>
2026-02-12 07:41:14 +0530

    adds godoc comments to package and function

--- add.go
+++ add.go
@@ -1,5 +1,8 @@
+// Package addgomodule provides method to add to numbers
 package addgomodule
 
+// Add adds two numbers [mathisfun]
+// [mathisfun]: https://www.mathisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
