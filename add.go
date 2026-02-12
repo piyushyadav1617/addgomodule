a974a329946d2a96d778a3a890bf257559516748
piyushyadav <yadavpiyush222@gmail.com>
2026-02-12 08:19:17 +0530

    makes Add generic

--- add.go
+++ add.go
@@ -1,8 +1,18 @@
 // Package addgomodule provides method to add to numbers
 package addgomodule
 
-// Add adds two numbers [mathisfun]
-// [mathisfun]: https://www.mathisfun.com/numbers/addition.html
-func Add(a, b int) int {
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add adds two numbers.
+// See [Math is Fun] for more about addition.
+//
+// [Math is Fun]: https://www.mathisfun.com/numbers/addition.html
+func Add[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/piyushyadav1617/addgomodule
 
 go 1.25.5
+
+require golang.org/x/exp v0.0.0-20260211191109-2735e65f0518 // indirect
